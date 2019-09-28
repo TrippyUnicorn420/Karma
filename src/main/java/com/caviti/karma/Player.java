2738069497916ba6031ae03085362b83b746b006
Siku Mahayi <smahayi31@gmail.com>
2019-09-28 13:57:46 +0200

    Added and fully documented Player object

---
+++ src/main/java/com/caviti/karma/Player.java
@@ -0,0 +1,40 @@
+package com.caviti.karma;
+
+import java.util.List;
+
+/**
+ * Code for the Player object.
+ * 
+ * @author Siku Mahayi, g19m0934@campus.ru.ac.za
+ */
+public class Player {
+
+    private List<Card> handCards;
+    private List<Card> tableCards;
+    
+    public Player() {
+        
+    }
+    
+    /**
+     * Give the cards to a player. 
+     * 
+     * @param handCards
+     * @param tableCards 
+     */
+    public void setCards(List<Card> handCards, List<Card> tableCards) {
+        this.handCards = handCards;
+        this.tableCards = tableCards;
+    }
+    
+    /**
+     * Make a play. Remove cards from your hand, and the game overseer handles
+     * what happens depending on what card(s) are played.
+     * 
+     * @param cards 
+     */
+    public void play(Card... cards) {
+        // Waiting on how Tim is going to handle plays being made
+    }
+    
+}
